4683150207cbdd3752f5761e3ba392cd4e400517
MechiScattini <mechiscatt@gmail.com>
2022-12-05 19:01:21 -0300

    intento de ABM users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,10 @@
 class UserController < ApplicationController
   load_and_authorize_resource
   def index
+    @users = User.all
   end
+
+  def show
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/index'
   resources :appointments
   devise_for :users
+  resources :users, only: [:index, :show]
   root 'home#index'
   resources :branches
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
